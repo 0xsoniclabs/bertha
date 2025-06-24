b87a9242261eb5f1c29c0827c43d97dbde24f434
Philip Salzmann <psalz@ph3.at>
2025-06-24 10:39:51 +0200

    Address Lorenz feedback, fix working directory race condition in tests

--- blockservice/src/main.rs
+++ blockservice/src/main.rs
@@ -17,7 +17,7 @@ fn init(path: Option<impl AsRef<Path>>) -> Result<(), Box<dyn std::error::Error>
         .map(|p| p.as_ref().to_path_buf())
         .unwrap_or_else(|| Path::new("./").to_path_buf());
     let path = path.canonicalize()?;
-    let path = path.join(BLOCK_DB_NAME).as_path().to_path_buf();
+    let path = path.join(BLOCK_DB_NAME);
     println!("Initializing new block database at: {}", path.display());
     blockdb::RocksBlockDb::create(path)?;
     Ok(())
@@ -31,7 +31,7 @@ fn import(path: impl AsRef<Path>) -> Result<(), Box<dyn std::error::Error>> {
     let mut reader = BufReader::new(file);
     let mut genesis = Genesis::parse(&mut reader)?;
     let chain_id = genesis.chain_id();
-    let mut blocks = genesis.blocks();
+    let blocks = genesis.blocks();
 
     let mut uncompressed_bytes_written = 0;
     let mut block_count = 0;
@@ -49,21 +49,26 @@ fn import(path: impl AsRef<Path>) -> Result<(), Box<dyn std::error::Error>> {
         .progress_chars("#>-"),
     );
     let before = std::time::Instant::now();
-    while let Some(Ok(block)) = blocks.next() {
-        if block_count == 0 {
-            // We rely on the fact that blocks are stored in reverse order.
-            total_blocks = block.number + 1;
-            println!("Importing {total_blocks} blocks for chain ID {chain_id}");
-            progress_bar.set_length(total_blocks);
-        }
+    for result in blocks {
+        match result {
+            Ok(block) => {
+                if block_count == 0 {
+                    // We rely on the fact that blocks are stored in reverse order.
+                    total_blocks = block.number + 1;
+                    println!("Importing {total_blocks} blocks for chain ID {chain_id}");
+                    progress_bar.set_length(total_blocks);
+                }
 
-        // We use put_raw so we can count bytes.
-        let number = block.number;
-        let protoblock = blockservice::proto::Block::from(block).encode_to_vec();
-        uncompressed_bytes_written += protoblock.len();
-        db.put_raw(chain_id, number, &protoblock)?;
-        block_count += 1;
-        progress_bar.set_position(block_count);
+                // We use put_raw so we can count bytes.
+                let number = block.number;
+                let protoblock = blockservice::proto::Block::from(block).encode_to_vec();
+                uncompressed_bytes_written += protoblock.len();
+                db.put_raw(chain_id, number, &protoblock)?;
+                block_count += 1;
+                progress_bar.set_position(block_count);
+            }
+            Err(e) => return Err(e.into()),
+        }
     }
     let elapsed = before.elapsed();
     progress_bar.finish();
@@ -105,16 +110,43 @@ fn main() {
 
 #[cfg(test)]
 mod tests {
-    use std::env;
+    use std::{env, os::unix::fs::PermissionsExt, path::PathBuf};
 
     use super::*;
+
+    static WORKING_DIR_MUTEX: std::sync::Mutex<()> = std::sync::Mutex::new(());
+
+    /// A guard type to temporarily change the working directory while avoiding
+    /// race-conditions across concurrently executed tests.
+    struct ChangeWorkingDir<'a> {
+        _guard: std::sync::MutexGuard<'a, ()>,
+        prev: PathBuf,
+    }
+
+    impl<'a> ChangeWorkingDir<'a> {
+        fn new(path: impl AsRef<Path>) -> Self {
+            let guard = WORKING_DIR_MUTEX.lock().unwrap();
+            let prev = env::current_dir().unwrap();
+            env::set_current_dir(path).unwrap();
+            ChangeWorkingDir {
+                _guard: guard,
+                prev,
+            }
+        }
+    }
+
+    impl<'a> Drop for ChangeWorkingDir<'a> {
+        fn drop(&mut self) {
+            env::set_current_dir(&self.prev).unwrap();
+        }
+    }
 
     #[test]
     fn init_creates_db() {
         // No args: Create in current working directory
         {
             let tmpdir = tempfile::tempdir().unwrap();
-            env::set_current_dir(tmpdir.path()).unwrap();
+            let _cwd = ChangeWorkingDir::new(tmpdir.path());
             let args = Args::parse_from(["blockservice", "init"]);
             execute(args).unwrap();
             assert!(tmpdir.path().join(BLOCK_DB_NAME).exists());
@@ -130,6 +162,40 @@ mod tests {
     }
 
     #[test]
+    fn init_fails_if_db_already_exists() {
+        let tmpdir = tempfile::tempdir().unwrap();
+        let _cwd = ChangeWorkingDir::new(tmpdir.path());
+        init(None::<&Path>).unwrap();
+
+        let args = Args::parse_from(["blockservice", "init"]);
+        let result = execute(args);
+        assert!(result.is_err());
+        assert!(
+            result
+                .unwrap_err()
+                .to_string()
+                .contains("error_if_exists is true")
+        );
+    }
+
+    #[test]
+    fn init_fails_if_no_write_permissions() {
+        let tmpdir = tempfile::tempdir().unwrap();
+        std::fs::set_permissions(&tmpdir.path(), std::fs::Permissions::from_mode(0o555)).unwrap();
+
+        let _cwd = ChangeWorkingDir::new(tmpdir.path());
+        let args = Args::parse_from(["blockservice", "init"]);
+        let result = execute(args);
+        assert!(result.is_err());
+        assert!(
+            result
+                .unwrap_err()
+                .to_string()
+                .contains("Permission denied")
+        );
+    }
+
+    #[test]
     fn import_inserts_all_blocks_from_snapshot_file_into_db() {
         let tmpdir = tempfile::tempdir().unwrap();
         let genesis_file = tmpdir.path().join("genesis.g");
@@ -138,7 +204,7 @@ mod tests {
         let genesis_data = genesis_parser::test_utils::generate_test_genesis(chain_id, num_blocks);
         std::fs::write(&genesis_file, genesis_data).unwrap();
 
-        env::set_current_dir(tmpdir.path()).unwrap();
+        let _cwd = ChangeWorkingDir::new(tmpdir.path());
         init(None::<&Path>).unwrap();
         let args = Args::parse_from(["blockservice", "import", genesis_file.to_str().unwrap()]);
         execute(args).unwrap();
@@ -149,5 +215,52 @@ mod tests {
             let block = db.get(chain_id, i as u64).unwrap();
             assert!(block.is_some(), "Block {i} not found in the database");
         }
+    }
+
+    #[test]
+    fn import_aborts_on_invalid_snapshot_file() {
+        let tmpdir = tempfile::tempdir().unwrap();
+        let genesis_file = tmpdir.path().join("genesis.g");
+        let mut genesis_data = genesis_parser::test_utils::generate_test_genesis(0, 5);
+        let data_len = genesis_data.len();
+        let corruption = [0xde, 0xad, 0xbe, 0xef];
+        genesis_data[data_len - corruption.len()..].copy_from_slice(&corruption); // Corrupt the last part of the file
+        std::fs::write(&genesis_file, genesis_data).unwrap();
+
+        let _cwd = ChangeWorkingDir::new(tmpdir.path());
+        init(None::<&Path>).unwrap();
+        let args = Args::parse_from(["blockservice", "import", genesis_file.to_str().unwrap()]);
+        let result = execute(args);
+        assert!(result.is_err());
+        assert!(
+            result
+                .unwrap_err()
+                .to_string()
+                .contains("corrupt gzip stream")
+        );
+    }
+
+    #[test]
+    fn import_aborts_on_database_error() {
+        let tmpdir = tempfile::tempdir().unwrap();
+        let genesis_file = tmpdir.path().join("genesis.g");
+        let genesis_data = genesis_parser::test_utils::generate_test_genesis(0, 5);
+        std::fs::write(&genesis_file, genesis_data).unwrap();
+
+        // Create a read-only database
+        let _cwd = ChangeWorkingDir::new(tmpdir.path());
+        init(None::<&Path>).unwrap();
+        let db_path = tmpdir.path().join(BLOCK_DB_NAME);
+        std::fs::set_permissions(&db_path, std::fs::Permissions::from_mode(0o555)).unwrap();
+
+        let args = Args::parse_from(["blockservice", "import", genesis_file.to_str().unwrap()]);
+        let result = execute(args);
+        assert!(result.is_err());
+        assert!(
+            result
+                .unwrap_err()
+                .to_string()
+                .contains("Permission denied")
+        );
     }
 }
