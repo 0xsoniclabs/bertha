72804a819c9c481128df2bd960c192f58b2413a4
ihtefyw <ph3work@proton.me>
2025-06-26 14:50:56 +0200

    Make data field in block always 12 bytes

--- go/test_data_generator/block.go
+++ go/test_data_generator/block.go
@@ -12,7 +12,6 @@ import (
 // blockHeaderFieldCases contains the corner cases for the fields of a block header.
 var blockHeaderFieldCases = map[string][]any{
 	"Extra": {
-		[]byte{},
 		[]byte{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11}, // 12 bytes
 	},
 }
