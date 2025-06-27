1323280e09ab776b0302fc224f73c2f808b771e1
Herbert Jordan <herbert@swrnd.ch>
2025-06-27 09:23:24 +0200

    Add Go based command line util to verify block hashes

---
+++ block-db-go-bindings/cmd/block-db/app/verify.go
@@ -0,0 +1,137 @@
+package app
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"iter"
+	"math"
+
+	"github.com/0xsoniclabs/blockdb"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/schollz/progressbar/v3"
+	"github.com/urfave/cli/v3"
+)
+
+var (
+	directoryFlag = &cli.StringFlag{
+		Name:    "database-dir",
+		Aliases: []string{"db"},
+		Usage:   "Path to the block database directory",
+		Value:   "./.blockdb",
+	}
+
+	chainIdFlag = &cli.Uint64Flag{
+		Name:    "chain-id",
+		Aliases: []string{"c"},
+		Usage:   "Chain ID to verify",
+		Value:   146, // Default to Sonic mainnet chain ID
+	}
+
+	startBlockFlag = &cli.Uint64Flag{
+		Name:    "start-block",
+		Aliases: []string{"s"},
+		Usage:   "Starting block number to verify",
+		Value:   0,
+	}
+
+	endBlockFlag = &cli.Uint64Flag{
+		Name:    "end-block",
+		Aliases: []string{"e"},
+		Usage:   "Ending block number to verify (inclusive)",
+		Value:   math.MaxUint64, // Default to the maximum block number
+	}
+)
+
+func getVerifyCommand() *cli.Command {
+	return &cli.Command{
+		Name:   "verify",
+		Usage:  "Verify the block database",
+		Action: runVerify,
+		Flags: []cli.Flag{
+			directoryFlag,
+			chainIdFlag,
+			startBlockFlag,
+			endBlockFlag,
+		},
+	}
+}
+
+func runVerify(ctx context.Context, c *cli.Command) (err error) {
+
+	dir := c.String(directoryFlag.Name)
+	chainId := c.Uint64(chainIdFlag.Name)
+	startBlock := c.Uint64(startBlockFlag.Name)
+	endBlock := c.Uint64(endBlockFlag.Name)
+
+	fmt.Printf("Opening block database in %q ...\n", dir)
+	database, err := blockdb.OpenDB(dir)
+	if err != nil {
+		return fmt.Errorf("failed to open database: %w", err)
+	}
+	defer func() {
+		err = errors.Join(err, database.Close())
+	}()
+
+	fmt.Printf("Verifying blocks for chain ID %d from block %d to block %d ...\n", chainId, startBlock, endBlock)
+
+	numBlocks := int64(endBlock - startBlock)
+	bar := progressbar.Default(numBlocks, "Verifying blocks")
+
+	return verifyBlocks(ctx, database.GetRangeRev(
+		chainId,
+		startBlock,
+		endBlock,
+	), func(uint64) {
+		_ = bar.Add(1) // Progress bar update errors are ignored
+	})
+}
+
+func verifyBlocks(
+	ctx context.Context,
+	blocks iter.Seq2[*blockdb.Block, error],
+	onVerifiedBlock func(number uint64),
+) error {
+	first := true
+	blockHash := common.Hash{}
+	for block, err := range blocks {
+		if ctx.Err() != nil {
+			return ctx.Err()
+		}
+		if err != nil {
+			return fmt.Errorf("failed to get block: %w", err)
+		}
+		if block == nil {
+			return fmt.Errorf("encountered nil block")
+		}
+
+		if !first {
+			if err := verifyBlock(blockHash, block); err != nil {
+				return fmt.Errorf("block verification failed for block %d: %w", block.Number, err)
+			}
+		}
+		if onVerifiedBlock != nil {
+			onVerifiedBlock(block.Number)
+		}
+		first = false
+		copy(blockHash[:], block.ParentHash)
+	}
+	return nil
+}
+
+func verifyBlock(
+	hash common.Hash,
+	block *blockdb.Block,
+) error {
+	gethBlock, err := ConvertToGethBlock(block)
+	if err != nil {
+		return fmt.Errorf("failed to convert block to Ethereum format: %w", err)
+	}
+
+	got := gethBlock.Hash()
+	if got != hash {
+		return fmt.Errorf("block hash mismatch: expected %x, got %x", hash, got)
+	}
+
+	return nil
+}
--- block-db-go-bindings/go.mod
+++ block-db-go-bindings/go.mod
@@ -7,6 +7,8 @@ require github.com/linxGnu/grocksdb v1.8.12
 require (
 	github.com/ethereum/go-ethereum v1.16.0
 	github.com/holiman/uint256 v1.3.2
+	github.com/schollz/progressbar/v3 v3.18.0
+	github.com/urfave/cli/v3 v3.3.8
 	google.golang.org/protobuf v1.36.6
 )
 
@@ -24,11 +26,12 @@ require (
 	github.com/gofrs/flock v0.12.1 // indirect
 	github.com/golang/snappy v0.0.5-0.20220116011046-fa5810519dcb // indirect
 	github.com/klauspost/cpuid/v2 v2.0.9 // indirect
-	github.com/mattn/go-runewidth v0.0.13 // indirect
+	github.com/mattn/go-runewidth v0.0.16 // indirect
 	github.com/minio/sha256-simd v1.0.0 // indirect
+	github.com/mitchellh/colorstring v0.0.0-20190213212951-d06e56a500db // indirect
 	github.com/mitchellh/mapstructure v1.4.1 // indirect
 	github.com/olekukonko/tablewriter v0.0.5 // indirect
-	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/shirou/gopsutil v3.21.4-0.20210419000835-c7a38de76ee5+incompatible // indirect
 	github.com/supranational/blst v0.3.14 // indirect
 	github.com/tklauser/go-sysconf v0.3.12 // indirect
@@ -36,6 +39,7 @@ require (
 	golang.org/x/crypto v0.36.0 // indirect
 	golang.org/x/sync v0.12.0 // indirect
 	golang.org/x/sys v0.31.0 // indirect
+	golang.org/x/term v0.30.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
 
