7890a8338fa7c066efc7d11cbf6d853a868405c8
Luigi PH3 <ph3work@proton.me>
2025-07-09 10:57:42 +0200

    Update blockservice/src/grpc/server.rs
    Co-authored-by: Lorenz Schüler <lorenz@saarow.de>

--- blockservice/src/grpc/server.rs
+++ blockservice/src/grpc/server.rs
@@ -351,7 +351,7 @@ mod tests {
         let listener = tokio::net::TcpListener::bind("[::1]:0").await.unwrap();
         let addr = listener.local_addr().unwrap();
         let job = tokio::spawn(async move {
-            let _ = server.serve(listener).await;
+            start(listener).await.unwrap();
         });
 
         let client = RpcClient::try_new(format!("http://{addr}").parse().unwrap()).await;
