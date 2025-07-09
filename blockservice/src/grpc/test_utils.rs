f3f5bffcd1a8c21cdd21fad3c152004cc23b09a3
Luigi PH3 <ph3work@proton.me>
2025-07-09 14:57:16 +0200

    Rename `DestructibleServer` to `TestServer` + remove start/shutdown functions (#76)

--- blockservice/src/grpc/test_utils.rs
+++ blockservice/src/grpc/test_utils.rs
@@ -2,6 +2,7 @@ use std::vec::IntoIter;
 
 use hyper_util::rt::TokioIo;
 use mockall::mock;
+use tokio::net::TcpListener;
 use tonic::transport::{Endpoint, Server, Uri};
 use tower::service_fn;
 
@@ -21,68 +22,63 @@ mock!(
 
     #[tonic::async_trait]
     impl BlockRpc for RpcServer {
-    async fn get_block(
-        &self,
+        async fn get_block(
+            &self,
             request: tonic::Request<BlockRequest>,
         ) -> Result<tonic::Response<EncodedBlock>, tonic::Status>;
 
-        // NOTE: mock! cannot find this name, so we ignore it and manually add it to get_block_range
-    type GetBlockRangeStream = futures::stream::Iter<IntoIter<Result<EncodedBlock, tonic::Status>>>;
+        // NOTE: [GetBlockRangeStream] is an associated type of [BlockRpc] and used as the return type of the `get_block_range` function. For some reason, the mock! macro appears to be unable to find this type. Because of this, we simply manually expand the return type of `get_block_range`.
+        type GetBlockRangeStream = futures::stream::Iter<IntoIter<Result<EncodedBlock, tonic::Status>>>;
 
         #[allow(clippy::type_complexity)]
-    async fn get_block_range(
-        &self,
+        async fn get_block_range(
+            &self,
             request: tonic::Request<BlockRangeRequest>,
         ) -> Result<tonic::Response<futures::stream::Iter<IntoIter<Result<EncodedBlock, tonic::Status>>>>, tonic::Status>;
 
-    async fn list(
-        &self,
+        async fn list(
+            &self,
             request: tonic::Request<ListRequest>,
         ) -> Result<tonic::Response<ChainRanges>, tonic::Status>;
     }
 );
 
-/// A mock server that can be stopped and polled for readiness.
-/// This is useful for testing commands where the client is constructed from an HTTP URI.
-pub struct DestructibleServer {
-    start_channel: tokio::sync::oneshot::Sender<()>,
+/// A server that can be used to spawn a mock gRPC server for testing purposes.
+pub struct TestServer {
     end_channel: tokio::sync::oneshot::Receiver<()>,
+    pub address: String,
 }
 
-impl DestructibleServer {
-    /// Construct a new `DestructibleServer` with the given mock server and initialize the
-    /// communication channels.
-    pub fn new(mock_server: MockRpcServer) -> Self {
-        // Create a pair of duplex streams to simulate the server and client communication
-        let (start_tx, mut start_rc) = tokio::sync::oneshot::channel();
+impl TestServer {
+    /// Start a new [TestServer] with the provided mock server on a random available port.
+    pub async fn new(mock_server: MockRpcServer) -> Self {
+        let listener = TcpListener::bind("127.0.0.1:0").await.unwrap();
+        let addr = listener.local_addr().unwrap();
         let (mut end_tx, end_rc) = tokio::sync::oneshot::channel();
         tokio::spawn(async move {
             Server::builder()
                 .add_service(BlockRpcServer::new(mock_server))
-                .serve_with_shutdown("[::1]:50051".parse().unwrap(), async move {
-                    // Notify that the server has started
-                    start_rc.close();
-                    // Wait for the shutdown signal
-                    end_tx.closed().await;
-                })
+                .serve_with_incoming_shutdown(
+                    tokio_stream::wrappers::TcpListenerStream::new(listener),
+                    async move {
+                        // Wait for the shutdown signal
+                        end_tx.closed().await;
+                    },
+                )
                 .await
                 .unwrap();
         });
 
-        DestructibleServer {
-            start_channel: start_tx,
+        TestServer {
             end_channel: end_rc,
+            address: format!("http://{addr}"),
         }
     }
+}
 
-    /// A function to wait for the server to start.
-    pub async fn wait_for_start(&mut self) {
-        // Wait for the server to start
-        self.start_channel.closed().await;
-    }
-
-    /// Shutdown and consumes the server
-    pub fn shutdown(mut self) {
+impl Drop for TestServer {
+    /// Drop the [TestServer] and close the end channel to signal shutdown.
+    fn drop(&mut self) {
         self.end_channel.close();
     }
 }
@@ -117,4 +113,30 @@ pub async fn get_mock_server_and_client(mock_server: MockRpcServer) -> RpcClient
         .unwrap();
 
     RpcClient::new(BlockRpcClient::new(channel))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    // NOTE: This test requires to be run with the `current_thread` runtime flavor to be sure that
+    // tokio::task::yield_now() gives execution back to the server task, allowing it to shut down
+    // properly.
+    #[tokio::test(flavor = "current_thread")]
+    async fn test_server_does_not_leak_server_after_drop() {
+        let server = TestServer::new(MockRpcServer::new()).await;
+        let url = server.address.clone();
+        {
+            let res = BlockRpcClient::connect(url.clone()).await;
+            assert!(res.is_ok(), "Client should connect to the server");
+        }
+        drop(server);
+
+        // Ensure the server has shut down
+        tokio::task::yield_now().await;
+        {
+            let client = BlockRpcClient::connect(url.clone()).await;
+            assert!(client.is_err(), "Client should not connect after shutdown");
+        }
+    }
 }
