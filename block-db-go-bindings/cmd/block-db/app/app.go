5023012e25144e30fa08849d12492cac26e47fd7
Herbert Jordan <herbert@swrnd.ch>
2025-07-11 09:23:45 +0200

    Add block replay tool

--- block-db-go-bindings/cmd/block-db/app/app.go
+++ block-db-go-bindings/cmd/block-db/app/app.go
@@ -2,6 +2,10 @@ package app
 
 import (
 	"context"
+	"log/slog"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/urfave/cli/v3"
 )
@@ -15,7 +19,23 @@ func getApp() *cli.Command {
 		Name:  "block-db",
 		Usage: "Block Database CLI",
 		Commands: []*cli.Command{
+			getReplayCommand(),
 			getVerifyCommand(),
+		},
+		Before: func(ctx context.Context, _ *cli.Command) (context.Context, error) {
+			ctx, cancel := context.WithCancel(ctx)
+			go func() {
+				sigs := make(chan os.Signal, 1)
+				signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+				select {
+				case <-ctx.Done():
+					return
+				case <-sigs:
+					slog.Warn("Received interrupt signal")
+					cancel()
+				}
+			}()
+			return ctx, nil
 		},
 	}
 }
