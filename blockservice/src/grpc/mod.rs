4afd23ebf67b44d2c94955a3e27025f3fec461df
ihtefyw <ph3work@proton.me>
2025-07-21 12:02:56 +0200

    Change compression algorithm to Zstd

--- blockservice/src/grpc/mod.rs
+++ blockservice/src/grpc/mod.rs
@@ -7,5 +7,6 @@ pub mod test_utils;
 pub use client::RpcClient;
 pub use server::RpcServer;
 
+/// The compression algorithm used for gRPC messages.
 const GRPC_COMPRESSION_ALGORITHM: tonic::codec::CompressionEncoding =
-    tonic::codec::CompressionEncoding::Gzip;
+    tonic::codec::CompressionEncoding::Zstd;
