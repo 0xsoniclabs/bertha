558a61dc7aa839185d8096dcc008354e34a9ae25
ihtefyw <ph3work@proton.me>
2025-07-28 16:50:02 +0200

    Add `ConfigFileAddressBinder`

--- blockservice/src/cmd/mod.rs
+++ blockservice/src/cmd/mod.rs
@@ -1,4 +1,7 @@
-use std::fmt::Write;
+use std::{
+    fmt::Write,
+    net::{IpAddr, Ipv6Addr, SocketAddr},
+};
 
 pub use fetch::fetch;
 pub use fetch_state_updates::fetch_state_updates;
@@ -8,8 +11,11 @@ pub use init::init;
 pub use list::list;
 pub use purge::purge;
 pub use start::start;
+use tokio::net::TcpListener;
 pub use verify::verify;
 pub use view::view;
+
+use crate::app_dir::open_app_dir;
 
 mod fetch;
 mod fetch_state_updates;
@@ -39,4 +45,52 @@ pub fn make_progress_bar(total: u64) -> Result<ProgressBar, TemplateError> {
         .progress_chars("#>-"),
     );
     Ok(bar)
+}
+
+/// Interface for binding an address to a TCP listener.
+#[tonic::async_trait]
+pub trait AddressBinder {
+    async fn bind_address(self) -> Result<TcpListener, Box<dyn std::error::Error + Send + Sync>>;
+}
+
+/// An `AddressBinder` that uses the config file to bind the address.
+pub struct ConfigFileAddressBinder {
+    config_dir: std::path::PathBuf,
+}
+
+impl ConfigFileAddressBinder {
+    /// Creates a new `DefaultAddressBinder` with the specified config directory.
+    pub fn new(config_dir: std::path::PathBuf) -> Self {
+        Self { config_dir }
+    }
+}
+
+#[tonic::async_trait]
+impl AddressBinder for ConfigFileAddressBinder {
+    /// Binds the address using the port specified in the config file.
+    /// Returns a `TcpListener` bound to the specified address.
+    async fn bind_address(self) -> Result<TcpListener, Box<dyn std::error::Error + Send + Sync>> {
+        let port = {
+            let (cfg, _db) = open_app_dir(self.config_dir.clone(), true)?;
+            cfg.get_port()
+        };
+
+        let addr = SocketAddr::new(IpAddr::V6(Ipv6Addr::UNSPECIFIED), port);
+        TcpListener::bind(addr).await.map_err(Into::into)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::app_dir::init_app_dir;
+
+    #[tokio::test]
+    async fn config_file_address_binder_bind_address_binds_address_with_config_file_port() {
+        let temp_dir = tempfile::tempdir().unwrap();
+        init_app_dir(temp_dir.path(), std::io::sink()).unwrap();
+        let binder = ConfigFileAddressBinder::new(temp_dir.path().to_path_buf());
+        let listener = binder.bind_address().await.unwrap();
+        assert_eq!(listener.local_addr().unwrap().port(), 8080); // Default port
+    }
 }
