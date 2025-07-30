cf9c90aa0bcd342d6ed68301333cacf3038bc23e
Luigi PH3 <ph3work@proton.me>
2025-07-30 14:48:08 +0200

    Add `InputReader` trait (#133)

--- blockservice/src/cmd/purge.rs
+++ blockservice/src/cmd/purge.rs
@@ -1,6 +1,9 @@
-use std::{io::Write, path::Path};
+use std::{
+    io::{BufRead, Write},
+    path::Path,
+};
 
-use crate::{app_dir::open_app_dir, db::BlockDb};
+use crate::{app_dir::open_app_dir, db::BlockDb, utils::InputReader};
 
 /// Purges blocks from the local database for a specific chain.
 /// If `from` is not provided, it defaults to 0.
@@ -11,7 +14,7 @@ pub fn purge(
     from: Option<u64>,
     to: Option<u64>,
     mut writer: impl std::io::Write,
-    mut reader: impl std::io::BufRead,
+    reader: &impl InputReader,
 ) -> Result<(), Box<dyn std::error::Error>> {
     if chain_id == 0 {
         return Err("chain ID cannot be 0".into());
@@ -23,7 +26,7 @@ pub fn purge(
         "Are you sure you want to purge blocks for chain {chain_id}? (y/n): "
     )?;
     std::io::stdout().flush()?;
-    reader.read_line(&mut input)?;
+    reader.get_reader().read_line(&mut input)?;
     if matches!(input.trim(), "y" | "Y") {
         let (_cfg, db) = open_app_dir(app_dir, false)?;
         db.delete_range(chain_id, from.unwrap_or(0), to.unwrap_or(u64::MAX))?;
@@ -43,25 +46,15 @@ mod tests {
         utils::test_dir::{Permissions, TestDir},
     };
 
-    /// Helper function to simulate user confirmation for the purge command.
-    fn confirm_purge(bool: bool) -> impl std::io::BufRead {
-        let input = if bool { "y\n" } else { "n\n" };
-        Cursor::new(input)
-    }
+    static CONFIRM_PURGE: Cursor<&'static str> = Cursor::new("y\n");
+    static DENY_PURGE: Cursor<&'static str> = Cursor::new("n\n");
 
     #[test]
     fn fails_if_app_dir_is_not_initialized() {
         let tmpdir = TestDir::try_new(Permissions::ReadWrite).unwrap();
 
         let mut writer = Vec::new();
-        let result = purge(
-            tmpdir.path(),
-            1,
-            None,
-            None,
-            &mut writer,
-            confirm_purge(true),
-        );
+        let result = purge(tmpdir.path(), 1, None, None, &mut writer, &CONFIRM_PURGE);
         assert!(result.is_err());
         assert!(result.unwrap_err().to_string().contains(&format!(
             "no blockservice.toml found at {} - did you forget to run init?",
@@ -83,14 +76,7 @@ mod tests {
         tmpdir.set_permissions(Permissions::WriteOnly).unwrap();
 
         let mut writer = Vec::new();
-        let result = purge(
-            tmpdir.path(),
-            1,
-            None,
-            None,
-            &mut writer,
-            confirm_purge(true),
-        );
+        let result = purge(tmpdir.path(), 1, None, None, &mut writer, &CONFIRM_PURGE);
         assert!(result.is_err());
         assert!(
             result
@@ -110,14 +96,7 @@ mod tests {
         init_app_dir(tmpdir.path(), std::io::sink()).unwrap();
 
         let mut writer = Vec::new();
-        let result = purge(
-            tmpdir.path(),
-            0,
-            None,
-            None,
-            &mut writer,
-            confirm_purge(true),
-        );
+        let result = purge(tmpdir.path(), 0, None, None, &mut writer, &CONFIRM_PURGE);
         assert!(result.is_err());
         assert_eq!(result.unwrap_err().to_string(), "chain ID cannot be 0");
     }
@@ -131,17 +110,7 @@ mod tests {
         // From is None, to is None
         {
             let mut writer = Vec::new();
-            assert!(
-                purge(
-                    tmpdir.path(),
-                    1,
-                    None,
-                    None,
-                    &mut writer,
-                    confirm_purge(true)
-                )
-                .is_ok()
-            );
+            assert!(purge(tmpdir.path(), 1, None, None, &mut writer, &CONFIRM_PURGE).is_ok());
             assert_eq!(
                 String::from_utf8(writer).unwrap(),
                 format!("Are you sure you want to purge blocks for chain 1? (y/n): ")
@@ -150,17 +119,7 @@ mod tests {
         // From is Some, to is None
         {
             let mut writer = Vec::new();
-            assert!(
-                purge(
-                    tmpdir.path(),
-                    1,
-                    Some(0),
-                    None,
-                    &mut writer,
-                    confirm_purge(true)
-                )
-                .is_ok()
-            );
+            assert!(purge(tmpdir.path(), 1, Some(0), None, &mut writer, &CONFIRM_PURGE).is_ok());
             assert_eq!(
                 String::from_utf8(writer).unwrap(),
                 format!("Are you sure you want to purge blocks for chain 1? (y/n): ")
@@ -176,7 +135,7 @@ mod tests {
                     Some(0),
                     Some(1),
                     &mut writer,
-                    confirm_purge(true)
+                    &CONFIRM_PURGE
                 )
                 .is_ok()
             );
@@ -221,7 +180,7 @@ mod tests {
                 Some(1),
                 Some(2),
                 &mut writer,
-                confirm_purge(true),
+                &CONFIRM_PURGE,
             )
             .unwrap();
             assert_eq!(
@@ -245,7 +204,7 @@ mod tests {
                 Some(1),
                 None,
                 &mut writer,
-                confirm_purge(true),
+                &CONFIRM_PURGE,
             )
             .unwrap();
             assert_eq!(
@@ -269,7 +228,7 @@ mod tests {
                 None,
                 Some(2),
                 &mut writer,
-                confirm_purge(true),
+                &CONFIRM_PURGE,
             )
             .unwrap();
             assert_eq!(
@@ -293,7 +252,7 @@ mod tests {
                 None,
                 None,
                 &mut writer,
-                confirm_purge(true),
+                &CONFIRM_PURGE,
             )
             .unwrap();
             assert_eq!(
@@ -318,15 +277,8 @@ mod tests {
         db.put_raw(42, 1, vec![1, 2, 3].as_slice()).unwrap();
 
         let mut writer = Vec::new();
-        purge(
-            tmpdir.path(),
-            1,
-            None,
-            None,
-            &mut writer,
-            confirm_purge(false),
-        )
-        .expect("purge should succeed");
+        purge(tmpdir.path(), 1, None, None, &mut writer, &DENY_PURGE)
+            .expect("purge should succeed");
         assert_eq!(db.get_raw(42, 1).unwrap(), Some(vec![1, 2, 3]));
 
         assert_eq!(
@@ -348,8 +300,15 @@ mod tests {
         {
             set_elem();
             let mut writer = Vec::new();
-            purge(tmpdir.path(), 42, None, None, &mut writer, Cursor::new("y"))
-                .expect("purge should succeed");
+            purge(
+                tmpdir.path(),
+                42,
+                None,
+                None,
+                &mut writer,
+                &Cursor::new("y"),
+            )
+            .expect("purge should succeed");
             assert_eq!(
                 String::from_utf8(writer).unwrap(),
                 format!("Are you sure you want to purge blocks for chain 42? (y/n): ")
@@ -361,8 +320,15 @@ mod tests {
         {
             set_elem();
             let mut writer = Vec::new();
-            purge(tmpdir.path(), 42, None, None, &mut writer, Cursor::new("Y"))
-                .expect("purge should succeed");
+            purge(
+                tmpdir.path(),
+                42,
+                None,
+                None,
+                &mut writer,
+                &Cursor::new("Y"),
+            )
+            .expect("purge should succeed");
             assert_eq!(
                 String::from_utf8(writer).unwrap(),
                 format!("Are you sure you want to purge blocks for chain 42? (y/n): ")
--- blockservice/src/utils/mod.rs
+++ blockservice/src/utils/mod.rs
@@ -1,3 +1,32 @@
+use std::io::Cursor;
+
 pub mod ranges;
 #[cfg(test)]
 pub mod test_dir;
+
+/// A trait for getting a buffered reader from different input types.
+/// This trait is useful when the type from which the [`BufRead`] is produced implements [`Send`]
+/// and [`Sync`], but the [`std::io::BufRead`] type itself does not, because this way we can pass
+/// the original type around, and produce the buffered reader when needed.
+pub trait InputReader {
+    fn get_reader(&self) -> impl std::io::BufRead;
+}
+
+/// A implementation of [`InputReader`] for [`Cursor<T>`], which allows reading from in-memory data.
+impl<T> InputReader for Cursor<T>
+where
+    T: AsRef<[u8]> + Clone,
+{
+    /// Returns the cursor itself as a buffered reader.
+    fn get_reader(&self) -> impl std::io::BufRead {
+        self.clone()
+    }
+}
+/// A implementation of [`InputReader`] for [`std::io::Stdin`], which locks standard input and
+/// allows reading from it.
+impl InputReader for std::io::Stdin {
+    /// Lock the standard input and return it as a buffered reader.
+    fn get_reader(&self) -> impl std::io::BufRead {
+        self.lock()
+    }
+}
