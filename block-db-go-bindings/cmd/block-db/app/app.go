3027d200867fca1f2064652e4bb9a1fa38f7fdf1
Herbert Jordan <herbert@swrnd.ch>
2025-10-27 15:14:55 +0100

    Simplified tracy integration

--- block-db-go-bindings/cmd/block-db/app/app.go
+++ block-db-go-bindings/cmd/block-db/app/app.go
@@ -5,7 +5,6 @@ import (
 	"log/slog"
 	"os"
 	"os/signal"
-	"runtime"
 	"syscall"
 
 	"github.com/0xsoniclabs/tracy"
@@ -18,7 +17,6 @@ func Run(args []string) error {
 
 func getApp() *cli.Command {
 	var profiler *profiler
-	var tracyProfiler *tracyProfiler
 	return &cli.Command{
 		Name:  "block-db",
 		Usage: "Block Database CLI",
@@ -30,7 +28,7 @@ func getApp() *cli.Command {
 			cpuProfileFlag,
 		},
 		Before: func(ctx context.Context, cmd *cli.Command) (context.Context, error) {
-			tracyProfiler = startTracyProfiler()
+			tracy.StartupProfiler()
 			var err error
 			profiler, err = StartCpuProfile(cmd.String(cpuProfileFlag.Name))
 			if err != nil {
@@ -54,38 +52,8 @@ func getApp() *cli.Command {
 			if profiler != nil {
 				return profiler.Stop()
 			}
-			tracyProfiler.Stop()
+			tracy.ShutdownProfiler()
 			return nil
 		},
 	}
-}
-
-type tracyProfiler struct {
-	stop chan<- struct{}
-	done <-chan struct{}
-}
-
-func startTracyProfiler() *tracyProfiler {
-	stop := make(chan struct{})
-	done := make(chan struct{})
-	p := &tracyProfiler{
-		stop: stop,
-		done: done,
-	}
-	go func() {
-		// Not sure whether this is necessary, but this ensures that the tracy
-		// profiler is started and stopped on the same OS thread.
-		defer close(done)
-		runtime.LockOSThread()
-		defer runtime.UnlockOSThread()
-		tracy.StartupProfiler()
-		<-stop
-		tracy.ShutdownProfiler()
-	}()
-	return p
-}
-
-func (p *tracyProfiler) Stop() {
-	close(p.stop)
-	<-p.done
 }
