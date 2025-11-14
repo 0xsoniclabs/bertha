c545d4eb4884e70883549a32320514d9fd3c239e
Lorenz Schüler <lorenz@saarow.de>
2025-11-14 16:12:08 +0100

    Add script to simplify running bertha replay with tracy

---
+++ block-db-go-bindings/go-run-with-tracy.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Script to run go with tracy profiling enabled.
+# Usage: ./go-run-with-tracy.sh <go-run-args>
+# e.g. ./go-run-with-tracy.sh ./cmd/block-db replay -g sonic.json -db .blockdb --db-schema 6 --db-variant rust-memory
+# It is assumed that the tracy and carmen repositories are located next to bertha.
+
+# Exit on error.
+set -e
+
+TRACY_DIR=$(pwd)/../../tracy
+CARMEN_RUST_DIR=$(pwd)/../../carmen/rust
+BLOCK_DB_GO_BINDINGS_DIR=$(pwd)
+
+# Build tracy shared library.
+cd $TRACY_DIR
+git submodule update --recursive --init
+make
+
+# Build carmen with tracy support.
+cd $CARMEN_RUST_DIR
+TRACY_CLIENT_LIB=TracyClient TRACY_CLIENT_LIB_PATH=$TRACY_DIR/tracy/build cargo build --release --features tracy
+
+# Override the go tracy and carmen dependencies to use local modified versions.
+# Check if the diff can be applied cleanly in reverse. In this case it is already applied.
+# Otherwise apply the diff
+cd $BLOCK_DB_GO_BINDINGS_DIR
+git apply --reverse --check enable-tracy-carmen.patch 2> /dev/null || git apply enable-tracy-carmen.patch
+
+# Run go with tracy tag.
+go run --tags enable_tracy "$@"
