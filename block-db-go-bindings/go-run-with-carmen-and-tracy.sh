69ec660263ba557b5cd910a4548c0b449fa8215b
ihtefyw <ph3work@proton.me>
2025-11-24 15:33:38 +0100

    Split tracy and carmen patch and add CI action to detect if they don't apply

--- block-db-go-bindings/go-run-with-carmen-and-tracy.sh
+++ block-db-go-bindings/go-run-with-carmen-and-tracy.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-# Script to run go with tracy profiling enabled.
-# Usage: ./go-run-with-tracy.sh <go-run-args>
-# e.g. ./go-run-with-tracy.sh ./cmd/block-db replay -g sonic.json -db .blockdb --db-schema 6 --db-variant rust-memory
+# Script to run go with local carmen modifications and tracy profiling enabled.
+# Usage: ./go-run-with-carmen-and-tracy.sh <go-run-args>
+# e.g. ./go-run-with-carmen-and-tracy.sh ./cmd/block-db replay -g sonic.json -db .blockdb --db-schema 6 --db-variant rust-memory
 # It is assumed that the tracy and carmen repositories are located next to bertha.
 
 # Exit on error.
 set -e
 # Print all commands before executing.
 set -x
+
+# Revert the patches on exit.
+trap 'git apply --reverse enable-carmen.patch &> /dev/null && git apply --reverse enable-tracy.patch &> /dev/null' EXIT
 
 TRACY_DIR=$(pwd)/../../tracy
 CARMEN_RUST_DIR=$(pwd)/../../carmen/rust
@@ -27,10 +30,8 @@ TRACY_CLIENT_LIB=TracyClient TRACY_CLIENT_LIB_PATH=$TRACY_DIR/tracy/build cargo 
 # Check if the diff can be applied cleanly in reverse. In this case it is already applied.
 # Otherwise apply the diff
 cd $BLOCK_DB_GO_BINDINGS_DIR
-git apply --reverse --check enable-tracy-carmen.patch 2> /dev/null || git apply enable-tracy-carmen.patch
+git apply --reverse --check enable-carmen.patch 2> /dev/null || git apply enable-carmen.patch
+git apply --reverse --check enable-tracy.patch 2> /dev/null || git apply enable-tracy.patch
 
 # Run go with tracy tag.
 go run --tags enable_tracy "$@"
-
-# Undo the patch to keep the git status clean.
-git apply --reverse enable-tracy-carmen.patch
